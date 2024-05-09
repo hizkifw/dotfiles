4b77b2800cf646290a537ad92991ad62d55f7f8e
Hizkia Felix <felixh@cynapse.ai>
2024-05-09 13:35:43 +0800

    update nvim plugins

--- dot/nvim/init.lua
+++ dot/nvim/init.lua
@@ -104,11 +104,11 @@ require('lazy').setup({
   {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
+    main = "ibl",
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
+      indent = {
+        char = '┊',
+      },
     },
   },
 
